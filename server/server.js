d82acee7b542746031d532628447ca99e0589415
Rohan Biradar <biradar.ro@northeastern.edu>
2023-11-21 16:15:32 -0500

    Adding create and Update DeliveryExecutive REST API

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import dotenv from "dotenv"
 import { connectDB } from "./database/connectDb.js";
 import userRouter from "./routes/userRoute.js"; 
+import deliveryRoutes from "./routes/deliveryRoutes.js";
 
 
 // Used to access variables from .env
@@ -28,6 +29,7 @@ app.use(morgan("dev"));
 
 // allows app to use auth routes
 app.use("/auth", userRouter);
+app.use("/delivery", deliveryRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
