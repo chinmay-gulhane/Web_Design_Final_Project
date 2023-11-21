3b1993eef002b2b62e89aa0fdbf50b29baf67c88
Chinmay Gulhane <chinmay.gulhane@gmail.com>
2023-11-21 15:21:06 -0500

    Updated Order Control Get All API

--- server/routes/order-route.js
+++ server/routes/order-route.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import * as orderController from '../controllers/order/order-controller.js';
+import express from "express";
+import * as orderController from "../controllers/order/order-controller.js";
 
 const router = express.Router();
 //imports controller and initialize object
 //registering the routes.
 //if http req method is get then find will be invoked
-router.route('/')
-    .get(orderController.find)
-    .post(orderController.post);
+router.route("/").get(orderController.find).post(orderController.post);
 
-router.route('/:id')
-    .get(orderController.get)
-    .put(orderController.put)
-    .delete(orderController.remove)
-    
+router
+  .route("/:id")
+  .get(orderController.get)
+  .put(orderController.put)
+  .delete(orderController.remove);
+
+router.route("/search").get(orderController.search);
 
 export default router;
--- server/services/Order/order-service.js
+++ server/services/Order/order-service.js
@@ -26,7 +26,29 @@ export const remove = async (id) => {
 };
 
 // get all orders
-export const search = async (params = {}) => {
+export const getAll = async (params = {}) => {
   const orders = await Order.find(params).exec(); //return promise so using await
   return orders;
+};
+
+// search orders
+export const search = async (params = {}, page = 1, pageSize = 10) => {
+  try {
+    // Extract page and pageSize from the request query parameters
+    const { page: reqPage, pageSize: reqPageSize } = params;
+
+    // Use the extracted values or fallback to default values if not provided
+    const skip = ((reqPage || page) - 1) * (reqPageSize || pageSize);
+
+    const orders = await Order.find(params)
+      .skip(skip)
+      .limit(reqPageSize || pageSize)
+      .exec();
+
+    return orders;
+  } catch (error) {
+    // Handle errors (e.g., log, throw, or return a default value)
+    console.error("Error in orderService.search:", error);
+    throw error;
+  }
 };
