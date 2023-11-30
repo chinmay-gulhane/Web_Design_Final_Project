68d9d4aa3368749f54f18c751048c6a82d0f221e
ashish-badhe <badhe.a@northeastern.edu>
2023-11-30 11:46:01 -0500

    updated project structure

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Husky Bites App",
+  description: "A food delivery application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
