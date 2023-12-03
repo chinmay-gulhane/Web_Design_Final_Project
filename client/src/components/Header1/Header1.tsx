a7d4db3d0322620cc922196b05fbd7cefdcc0fa2
Rohan Biradar <biradar.ro@northeastern.edu>
2023-12-03 15:43:21 -0500

    adding header and footer

--- client/src/components/Header1/Header1.tsx
+++ client/src/components/Header1/Header1.tsx
@@ -15,10 +15,24 @@ import Menu from "@mui/material/Menu";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./header1.css";
+import Drawer from "@mui/material/Drawer";
+
 
 export default function MenuAppBar() {
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+  
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+  
+
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAuth(event.target.checked);
@@ -32,8 +46,18 @@ export default function MenuAppBar() {
     setAnchorEl(null);
   };
 
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 ,
+      backgroundColor: darkMode ? '#333' : 'inherit',
+      color: darkMode ? '#fff' : 'inherit',
+       }}>
       {/* <FormGroup>
         <FormControlLabel
           control={
@@ -54,9 +78,22 @@ export default function MenuAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>
+          <Drawer
+  anchor="left"
+  open={drawerOpen}
+  onClose={handleDrawerClose}
+>
+  <div className="drawer">
+    <MenuItem onClick={handleDrawerClose}>Manage Profile</MenuItem>
+    <MenuItem onClick={handleDrawerClose}>My Orders</MenuItem>
+    <MenuItem onClick={handleDrawerClose}>Help</MenuItem>
+  </div>
+</Drawer>
+
           <Typography
             variant="h6"
             component="div"
@@ -68,7 +105,7 @@ export default function MenuAppBar() {
             <span className="cart">
               <ShoppingCartIcon />
             </span>
-            <span className="dark-mode">
+            <span className="dark-mode" onClick={toggleDarkMode}>
               <DarkModeIcon />
             </span>
             
