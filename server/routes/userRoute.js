5914744230678f688352f1ec2b83cdd31a158622
ashish-badhe <badhe.a@northeastern.edu>
2023-12-13 13:47:12 -0500

    added deleteUser Api

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
 import {
-  getUsersController
+  getUsersController,
+  deleteUserController
 } from "../controllers/user-controller/userController.js";
 // import { userAuthenticationMiddleware } from "../middlewares/authMiddlerware.js";
 
 const router = express.Router();
 
 router.get("/getUsers", getUsersController);
+
+router.delete("/:id", deleteUserController);
 
 export default router;
